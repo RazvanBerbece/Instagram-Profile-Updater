138ae3accde39908803a3b0f8381ebd238a89d4e
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2020-12-08 19:15:27 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// TODO:
+// 1. use 3rd party lib wrapper for IG API -> get _mildlyinteresting_ UID
+// 2. use UID and (perhaps ?) use IG Graphs API to get media objects
+// 3. use media objects to calculate likes
+// 4. use 3rd party lib wrapper from 1) and update profile bio
+//
+// HAS TO BE DEPLOYED
